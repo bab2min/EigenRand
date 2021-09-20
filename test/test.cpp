72b041533732ea1b69e050c52e9774f8c5c561ef
Minchul Lee <bab2min@gmail.com>
2021-09-21 03:26:44 +0900

    added test cases

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,64 @@
 #include <gtest/gtest.h>
 #include <Eigen/Dense>
 #include <EigenRand/EigenRand>
+#include <cmath>
+
+TEST(MorePacketMath, log)
+{
+	Eigen::ArrayXf x(8);
+	x << 0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0;
+	Eigen::ArrayXf y = x.log();
+	std::cout << y.transpose() << std::endl;
+	for (int i = 0; i < x.size(); ++i)
+	{
+		if (std::isnan(std::log(x[i])))
+		{
+			EXPECT_TRUE(std::isnan(y[i]));
+		}
+		else
+		{
+			EXPECT_FLOAT_EQ(std::log(x[i]), y[i]);
+		}
+	}
+}
+
+TEST(MorePacketMath, sin)
+{
+	Eigen::ArrayXf x(8);
+	x << 0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0;
+	Eigen::ArrayXf y = x.sin();
+	std::cout << y.transpose() << std::endl;
+	for (int i = 0; i < x.size(); ++i)
+	{
+		if (std::isnan(std::sin(x[i])))
+		{
+			EXPECT_TRUE(std::isnan(y[i]));
+		}
+		else
+		{
+			EXPECT_FLOAT_EQ(std::sin(x[i]), y[i]);
+		}
+	}
+}
+
+TEST(MorePacketMath, sqrt)
+{
+	Eigen::ArrayXf x(8);
+	x << 0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0;
+	Eigen::ArrayXf y = x.sqrt();
+	std::cout << y.transpose() << std::endl;
+	for (int i = 0; i < x.size(); ++i)
+	{
+		if (std::isnan(std::sqrt(x[i])))
+		{
+			EXPECT_TRUE(std::isnan(y[i]));
+		}
+		else
+		{
+			EXPECT_FLOAT_EQ(std::sqrt(x[i]), y[i]);
+		}
+	}
+}
 
 template <class T>
 class ContinuousDistTest : public testing::Test
@@ -93,6 +151,238 @@ TYPED_TEST(ContinuousDistTest, exponential)
 	mat = Eigen::Rand::exponential<Matrix>(1, 3, gen, 2);
 	std::cout << mat << std::endl;
 }
+
+TYPED_TEST(ContinuousDistTest, lognormal)
+{
+	using Matrix = Eigen::Matrix<TypeParam, -1, -1>;
+	Eigen::Rand::Vmt19937_64 gen{ 42 };
+	Matrix mat;
+
+	mat = Eigen::Rand::lognormal<Matrix>(8, 8, gen, 1, 2);
+	mat = Eigen::Rand::lognormal<Matrix>(3, 3, gen, 1, 2);
+	mat = Eigen::Rand::lognormal<Matrix>(5, 5, gen, 1, 2);
+	mat = Eigen::Rand::lognormal<Matrix>(5, 3, gen, 1, 2);
+	mat = Eigen::Rand::lognormal<Matrix>(1, 3, gen, 1, 2);
+	std::cout << mat << std::endl;
+}
+
+TYPED_TEST(ContinuousDistTest, beta)
+{
+	using Matrix = Eigen::Matrix<TypeParam, -1, -1>;
+	Eigen::Rand::Vmt19937_64 gen{ 42 };
+	Matrix mat;
+
+	mat = Eigen::Rand::beta<Matrix>(8, 8, gen, 1, 2);
+	mat = Eigen::Rand::beta<Matrix>(3, 3, gen, 1, 2);
+	mat = Eigen::Rand::beta<Matrix>(5, 5, gen, 1, 2);
+	mat = Eigen::Rand::beta<Matrix>(5, 3, gen, 1, 2);
+	mat = Eigen::Rand::beta<Matrix>(1, 3, gen, 1, 2);
+	std::cout << mat << std::endl;
+}
+
+TYPED_TEST(ContinuousDistTest, cauchy)
+{
+	using Matrix = Eigen::Matrix<TypeParam, -1, -1>;
+	Eigen::Rand::Vmt19937_64 gen{ 42 };
+	Matrix mat;
+
+	mat = Eigen::Rand::cauchy<Matrix>(8, 8, gen, 1, 2);
+	mat = Eigen::Rand::cauchy<Matrix>(3, 3, gen, 1, 2);
+	mat = Eigen::Rand::cauchy<Matrix>(5, 5, gen, 1, 2);
+	mat = Eigen::Rand::cauchy<Matrix>(5, 3, gen, 1, 2);
+	mat = Eigen::Rand::cauchy<Matrix>(1, 3, gen, 1, 2);
+	std::cout << mat << std::endl;
+}
+
+TYPED_TEST(ContinuousDistTest, studentT)
+{
+	using Matrix = Eigen::Matrix<TypeParam, -1, -1>;
+	Eigen::Rand::Vmt19937_64 gen{ 42 };
+	Matrix mat;
+
+	mat = Eigen::Rand::studentT<Matrix>(8, 8, gen, 5);
+	mat = Eigen::Rand::studentT<Matrix>(3, 3, gen, 5);
+	mat = Eigen::Rand::studentT<Matrix>(5, 5, gen, 5);
+	mat = Eigen::Rand::studentT<Matrix>(5, 3, gen, 5);
+	mat = Eigen::Rand::studentT<Matrix>(1, 3, gen, 5);
+	std::cout << mat << std::endl;
+}
+
+TYPED_TEST(ContinuousDistTest, gamma)
+{
+	using Matrix = Eigen::Matrix<TypeParam, -1, -1>;
+	Eigen::Rand::Vmt19937_64 gen{ 42 };
+	Matrix mat;
+
+	mat = Eigen::Rand::gamma<Matrix>(8, 8, gen, 1, 2);
+	mat = Eigen::Rand::gamma<Matrix>(3, 3, gen, 1, 2);
+	mat = Eigen::Rand::gamma<Matrix>(5, 5, gen, 1, 2);
+	mat = Eigen::Rand::gamma<Matrix>(5, 3, gen, 1, 2);
+	mat = Eigen::Rand::gamma<Matrix>(1, 3, gen, 1, 2);
+	std::cout << mat << std::endl;
+}
+
+TYPED_TEST(ContinuousDistTest, weibull)
+{
+	using Matrix = Eigen::Matrix<TypeParam, -1, -1>;
+	Eigen::Rand::Vmt19937_64 gen{ 42 };
+	Matrix mat;
+
+	mat = Eigen::Rand::weibull<Matrix>(8, 8, gen, 1, 2);
+	mat = Eigen::Rand::weibull<Matrix>(3, 3, gen, 1, 2);
+	mat = Eigen::Rand::weibull<Matrix>(5, 5, gen, 1, 2);
+	mat = Eigen::Rand::weibull<Matrix>(5, 3, gen, 1, 2);
+	mat = Eigen::Rand::weibull<Matrix>(1, 3, gen, 1, 2);
+	std::cout << mat << std::endl;
+}
+
+TYPED_TEST(ContinuousDistTest, extremeValueLike)
+{
+	using Matrix = Eigen::Matrix<TypeParam, -1, -1>;
+	Eigen::Rand::Vmt19937_64 gen{ 42 };
+	Matrix mat;
+
+	mat = Eigen::Rand::extremeValue<Matrix>(8, 8, gen, 1, 2);
+	mat = Eigen::Rand::extremeValue<Matrix>(3, 3, gen, 1, 2);
+	mat = Eigen::Rand::extremeValue<Matrix>(5, 5, gen, 1, 2);
+	mat = Eigen::Rand::extremeValue<Matrix>(5, 3, gen, 1, 2);
+	mat = Eigen::Rand::extremeValue<Matrix>(1, 3, gen, 1, 2);
+	std::cout << mat << std::endl;
+}
+
+TYPED_TEST(ContinuousDistTest, chiSquared)
+{
+	using Matrix = Eigen::Matrix<TypeParam, -1, -1>;
+	Eigen::Rand::Vmt19937_64 gen{ 42 };
+	Matrix mat;
+
+	mat = Eigen::Rand::chiSquared<Matrix>(8, 8, gen, 7);
+	mat = Eigen::Rand::chiSquared<Matrix>(3, 3, gen, 7);
+	mat = Eigen::Rand::chiSquared<Matrix>(5, 5, gen, 7);
+	mat = Eigen::Rand::chiSquared<Matrix>(5, 3, gen, 7);
+	mat = Eigen::Rand::chiSquared<Matrix>(1, 3, gen, 7);
+	std::cout << mat << std::endl;
+}
+
+TYPED_TEST(ContinuousDistTest, fisherF1)
+{
+	using Matrix = Eigen::Matrix<TypeParam, -1, -1>;
+	Eigen::Rand::Vmt19937_64 gen{ 42 };
+	Matrix mat;
+
+	mat = Eigen::Rand::fisherF<Matrix>(8, 8, gen, 1, 1);
+	mat = Eigen::Rand::fisherF<Matrix>(3, 3, gen, 1, 1);
+	mat = Eigen::Rand::fisherF<Matrix>(5, 5, gen, 1, 1);
+	mat = Eigen::Rand::fisherF<Matrix>(5, 3, gen, 1, 1);
+	mat = Eigen::Rand::fisherF<Matrix>(1, 3, gen, 1, 1);
+	std::cout << mat << std::endl;
+}
+
+TYPED_TEST(ContinuousDistTest, fisherF2)
+{
+	using Matrix = Eigen::Matrix<TypeParam, -1, -1>;
+	Eigen::Rand::Vmt19937_64 gen{ 42 };
+	Matrix mat;
+
+	mat = Eigen::Rand::fisherF<Matrix>(8, 8, gen, 5, 5);
+	mat = Eigen::Rand::fisherF<Matrix>(3, 3, gen, 5, 5);
+	mat = Eigen::Rand::fisherF<Matrix>(5, 5, gen, 5, 5);
+	mat = Eigen::Rand::fisherF<Matrix>(5, 3, gen, 5, 5);
+	mat = Eigen::Rand::fisherF<Matrix>(1, 3, gen, 5, 5);
+	std::cout << mat << std::endl;
+}
+
+template <class T>
+class DiscreteDistTest : public testing::Test
+{
+};
+
+TYPED_TEST_CASE(DiscreteDistTest, testing::Types<int32_t>);
+
+TYPED_TEST(DiscreteDistTest, uniformInt)
+{
+	using Matrix = Eigen::Matrix<TypeParam, -1, -1>;
+	Eigen::Rand::Vmt19937_64 gen{ 42 };
+	Matrix mat;
+
+	mat = Eigen::Rand::uniformInt<Matrix>(8, 8, gen, 0, 9);
+	mat = Eigen::Rand::uniformInt<Matrix>(3, 3, gen, 0, 9);
+	mat = Eigen::Rand::uniformInt<Matrix>(5, 5, gen, 0, 9);
+	mat = Eigen::Rand::uniformInt<Matrix>(5, 3, gen, 0, 9);
+	mat = Eigen::Rand::uniformInt<Matrix>(1, 3, gen, 0, 9);
+	std::cout << mat << std::endl;
+}
+
+TYPED_TEST(DiscreteDistTest, discrete)
+{
+	using Matrix = Eigen::Matrix<TypeParam, -1, -1>;
+	Eigen::Rand::Vmt19937_64 gen{ 42 };
+	Matrix mat;
+
+	mat = Eigen::Rand::discrete<Matrix>(8, 8, gen, { 1, 2, 3, 4, 5, 6 });
+	mat = Eigen::Rand::discrete<Matrix>(3, 3, gen, { 1, 2, 3, 4, 5, 6 });
+	mat = Eigen::Rand::discrete<Matrix>(5, 5, gen, { 1, 2, 3, 4, 5, 6 });
+	mat = Eigen::Rand::discrete<Matrix>(5, 3, gen, { 1, 2, 3, 4, 5, 6 });
+	mat = Eigen::Rand::discrete<Matrix>(1, 3, gen, { 1, 2, 3, 4, 5, 6 });
+	std::cout << mat << std::endl;
+}
+
+TYPED_TEST(DiscreteDistTest, poisson)
+{
+	using Matrix = Eigen::Matrix<TypeParam, -1, -1>;
+	Eigen::Rand::Vmt19937_64 gen{ 42 };
+	Matrix mat;
+
+	mat = Eigen::Rand::poisson<Matrix>(8, 8, gen, 1);
+	mat = Eigen::Rand::poisson<Matrix>(3, 3, gen, 1);
+	mat = Eigen::Rand::poisson<Matrix>(5, 5, gen, 1);
+	mat = Eigen::Rand::poisson<Matrix>(5, 3, gen, 1);
+	mat = Eigen::Rand::poisson<Matrix>(1, 3, gen, 1);
+	std::cout << mat << std::endl;
+}
+
+TYPED_TEST(DiscreteDistTest, binomial)
+{
+	using Matrix = Eigen::Matrix<TypeParam, -1, -1>;
+	Eigen::Rand::Vmt19937_64 gen{ 42 };
+	Matrix mat;
+
+	mat = Eigen::Rand::binomial<Matrix>(8, 8, gen, 10, 0.5);
+	mat = Eigen::Rand::binomial<Matrix>(3, 3, gen, 10, 0.5);
+	mat = Eigen::Rand::binomial<Matrix>(5, 5, gen, 10, 0.5);
+	mat = Eigen::Rand::binomial<Matrix>(5, 3, gen, 10, 0.5);
+	mat = Eigen::Rand::binomial<Matrix>(1, 3, gen, 10, 0.5);
+	std::cout << mat << std::endl;
+}
+
+TYPED_TEST(DiscreteDistTest, negativeBinomial)
+{
+	using Matrix = Eigen::Matrix<TypeParam, -1, -1>;
+	Eigen::Rand::Vmt19937_64 gen{ 42 };
+	Matrix mat;
+
+	mat = Eigen::Rand::negativeBinomial<Matrix>(8, 8, gen, 10, 0.5);
+	mat = Eigen::Rand::negativeBinomial<Matrix>(3, 3, gen, 10, 0.5);
+	mat = Eigen::Rand::negativeBinomial<Matrix>(5, 5, gen, 10, 0.5);
+	mat = Eigen::Rand::negativeBinomial<Matrix>(5, 3, gen, 10, 0.5);
+	mat = Eigen::Rand::negativeBinomial<Matrix>(1, 3, gen, 10, 0.5);
+	std::cout << mat << std::endl;
+}
+
+TYPED_TEST(DiscreteDistTest, geometric)
+{
+	using Matrix = Eigen::Matrix<TypeParam, -1, -1>;
+	Eigen::Rand::Vmt19937_64 gen{ 42 };
+	Matrix mat;
+
+	mat = Eigen::Rand::geometric<Matrix>(8, 8, gen, 0.25);
+	mat = Eigen::Rand::geometric<Matrix>(3, 3, gen, 0.25);
+	mat = Eigen::Rand::geometric<Matrix>(5, 5, gen, 0.25);
+	mat = Eigen::Rand::geometric<Matrix>(5, 3, gen, 0.25);
+	mat = Eigen::Rand::geometric<Matrix>(1, 3, gen, 0.25);
+	std::cout << mat << std::endl;
+}
+
 
 TEST(Issue, 29)
 {
