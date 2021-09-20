f4e4a2fc86e7ecd5849901ac2a3febc9dbd9e38e
Minchul Lee <bab2min@gmail.com>
2021-09-20 19:09:28 +0900

    added test case for matrix with odd rows and cols (#29)

--- test/test.cpp
+++ test/test.cpp
@@ -20,6 +20,8 @@ TYPED_TEST(ContinuousDistTest, balanced)
 	mat = Eigen::Rand::balanced<Matrix>(8, 8, gen);
 	mat = Eigen::Rand::balanced<Matrix>(3, 3, gen);
 	mat = Eigen::Rand::balanced<Matrix>(5, 5, gen);
+	mat = Eigen::Rand::balanced<Matrix>(5, 3, gen);
+	mat = Eigen::Rand::balanced<Matrix>(1, 3, gen);
 	std::cout << mat << std::endl;
 }
 
@@ -32,6 +34,21 @@ TYPED_TEST(ContinuousDistTest, balanced2)
 	mat = Eigen::Rand::balanced<Matrix>(8, 8, gen, 0.5, 2);
 	mat = Eigen::Rand::balanced<Matrix>(3, 3, gen, 0.5, 2);
 	mat = Eigen::Rand::balanced<Matrix>(5, 5, gen, 0.5, 2);
+	mat = Eigen::Rand::balanced<Matrix>(5, 3, gen, 0.5, 2);
+	mat = Eigen::Rand::balanced<Matrix>(1, 3, gen, 0.5, 2);
+	std::cout << mat << std::endl;
+}
+
+TYPED_TEST(ContinuousDistTest, uniformReal)
+{
+	using Matrix = Eigen::Matrix<TypeParam, -1, -1>;
+	Eigen::Rand::Vmt19937_64 gen{ 42 };
+	Matrix mat;
+
+	mat = Eigen::Rand::uniformReal<Matrix>(8, 8, gen);
+	mat = Eigen::Rand::uniformReal<Matrix>(3, 3, gen);
+	mat = Eigen::Rand::uniformReal<Matrix>(5, 5, gen);
+	mat = Eigen::Rand::uniformReal<Matrix>(5, 3, gen);
 	std::cout << mat << std::endl;
 }
 
@@ -44,6 +61,8 @@ TYPED_TEST(ContinuousDistTest, stdNormal)
 	mat = Eigen::Rand::normal<Matrix>(8, 8, gen);
 	mat = Eigen::Rand::normal<Matrix>(3, 3, gen);
 	mat = Eigen::Rand::normal<Matrix>(5, 5, gen);
+	mat = Eigen::Rand::normal<Matrix>(5, 3, gen);
+	mat = Eigen::Rand::normal<Matrix>(1, 3, gen);
 	std::cout << mat << std::endl;
 }
 
@@ -56,6 +75,8 @@ TYPED_TEST(ContinuousDistTest, normal)
 	mat = Eigen::Rand::normal<Matrix>(8, 8, gen, 1, 2);
 	mat = Eigen::Rand::normal<Matrix>(3, 3, gen, 1, 2);
 	mat = Eigen::Rand::normal<Matrix>(5, 5, gen, 1, 2);
+	mat = Eigen::Rand::normal<Matrix>(5, 3, gen, 1, 2);
+	mat = Eigen::Rand::normal<Matrix>(1, 3, gen, 1, 2);
 	std::cout << mat << std::endl;
 }
 
@@ -68,5 +89,17 @@ TYPED_TEST(ContinuousDistTest, exponential)
 	mat = Eigen::Rand::exponential<Matrix>(8, 8, gen, 2);
 	mat = Eigen::Rand::exponential<Matrix>(3, 3, gen, 2);
 	mat = Eigen::Rand::exponential<Matrix>(5, 5, gen, 2);
+	mat = Eigen::Rand::exponential<Matrix>(5, 3, gen, 2);
+	mat = Eigen::Rand::exponential<Matrix>(1, 3, gen, 2);
 	std::cout << mat << std::endl;
+}
+
+TEST(Issue, 29)
+{
+	typedef Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::ColMajor> CMatrix;
+	using Eigen::Rand::Vmt19937_64;
+	using Eigen::Rand::uniformReal;
+
+	Vmt19937_64 gen_eigen;
+	CMatrix X = uniformReal<CMatrix>(5, 3, gen_eigen);
 }
